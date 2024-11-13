9777768656a6deb7c9c1c07526ce4cebf18849f0
Frédéric Pili <80117329+FredPili@users.noreply.github.com>
2024-11-13 19:04:59 +0100

    classify pipeline updated

--- classify.py
+++ classify.py
@@ -60,3 +60,52 @@ def classify_broad(data, categories, on, classifier, batch_size=16):
     print("Converting back to dataframe...")
     final_data = dataset.to_pandas()
     return final_data
+
+def classify_dataset(batch, candidate_labels, on, classifier, multi_label=False) :
+    texts = batch[on]
+    results = classifier(texts, candidate_labels, multi_label=multi_label)
+
+    labels_per_text = []
+    for result in results:
+        scores, labels = result['scores'], result['labels']
+        sorted_pairs = sorted(zip(scores, labels), key=lambda x: x[0], reverse=True)
+        scores, labels = zip(*sorted_pairs)
+
+        max_score = scores[0]
+        threshold = max_score * 0.9
+        top_count = len([score for score in scores if score >= threshold])
+
+        # Apply classification logic based on thresholds
+        if max_score < 0.3:
+            labels_per_text.append(["misc"])
+        elif top_count == 1:
+            labels_per_text.append([labels[0]])
+        elif top_count == 2 and multi_label:
+            labels_per_text.append([labels[0], labels[1]])
+        elif top_count == 3 and multi_label:
+            labels_per_text.append([labels[0], labels[1], labels[2]])
+        else:
+            labels_per_text.append(["uncertain"])
+
+    # Add the classification labels as a new column in the batch
+    batch["classified_labels"] = labels_per_text
+    return batch
+
+def classify(data, candidate_labels, classifier, on='text', multi_label=True, batch_size=16):
+    print('Convering to dataset...')
+    dataset = Dataset.from_pandas(data)
+    print('Processing...')
+    dataset = dataset.map(
+        classify_dataset, 
+        batched=True, 
+        batch_size=batch_size, 
+        fn_kwargs={'classifier': classifier, 
+                'candidate_labels': candidate_labels, 
+                'on': on, 
+                'multi_label': multi_label},
+    )
+    print('Converting back to DataFrame...')
+    final_data = dataset.to_pandas()
+    return final_data
+    
+
